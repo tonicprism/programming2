93236da253fe682e8cc804971d8ae307d6ab7afe
tonicprism <51770634+tonicprism@users.noreply.github.com>
2019-09-23 11:58:54 -0300

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -1,11 +1,37 @@
+package Ex06;
+import java.util.Scanner;
+public class Ex06 {
+	public static void main(String[] args) {
+		
+		// Lista de Heróis
+		String[] listaHerois = {"CapitaoAmerica", "HomemDeFerro"};
+		// Lista de Vilões
+		String[] listaViloes = {"CaveiraVermelha", "Ultron"};
+		
+		// Instancia da classe Scanner
+		Scanner s = new Scanner(System.in);
+		
+		// Entrada do scanner
+		System.out.println("Digite o nome de um heroi: ");
+		String busca = s.nextLine().toLowerCase();
+		
+		// Varredura de valores na lista
+		for(int i=0;i<10;i++) {
+			// Busca na lista dos Heróis
+			if(busca.equals(listaHerois[i].toLowerCase()))
+				{
+					System.out.println("É um héroi...");
+				}
+			// Busca na lista dos Vilões	
+			else if(busca.equals(listaViloes[i].toLowerCase()))
+				{
+					System.out.println("É um vilão... ");
+				}
+			else {
+				System.out.println("Ele não faz parte de nenhuma das listas...");
+			}
+			 }
+		
+		}
+	 }
 
-public class Ex6 {
- public static void main(String[] args) {
-	String[] listaHerois = {"CapitaoAmerica", "HomemDeFerro","ViuvaNegra", "Hulk", "HomemAranha", "Thor" };
-	String[] listaViloes = {"CaveiraVermelha", "Mandarim", "Treinador", "HulkVermelho", "DuendeVerde", "Loki" };
-	
-	for(int i = 0;i >= listaHerois.length;i++) {
-		System.out.println(listaHerois[i]);
-	}
- }
-}
