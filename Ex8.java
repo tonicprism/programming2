9e5b0626b06f7868f608a566e88a9972268c7f4b
tonicprism <51770634+tonicprism@users.noreply.github.com>
2019-09-28 19:02:06 -0300

    Update Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exerciciosmaratonap2;
 
 import java.util.Random;
 import java.util.Scanner;
